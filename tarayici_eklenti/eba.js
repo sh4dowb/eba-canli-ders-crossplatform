1f7093d2b5d6b59eb1e2cba652fcfbc5b8ed42bb
Çağrı <cagriari@pm.me>
2020-11-25 08:02:02 +0000

    mavi ekran harici için destek

--- tarayici_eklenti/eba.js
+++ tarayici_eklenti/eba.js
@@ -58,7 +58,73 @@ function startEba() {
                 }
             });
         } else {
-            alert('Eklentiyi mavi arkaplanlı canlı ders sayfasında çalıştırınız.')
+            $.ajax({
+                url: "https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//studytime/getstudentstudytime",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "json"
+                },
+                data: "status=1&type=2&pagesize=25&pagenumber=0",
+                withCredentials: true,
+                crossDomain: true,
+                xhrFields: {
+                    withCredentials: true
+                },
+                dataType: "json",
+                success: function(resp) {
+                    var result = resp.studyTimeList;
+                    var dersler = [];
+                    var dersText = "";
+                    var id = 1;
+                    for (var i in result) {
+                        if ((new Date).getTime() + 18000000 > result[i].startdate) {
+                            dersler.push(result[i]);
+                            dersText = dersText + (id.toString() + ") " + result[i].title + " (" + result[i].ownerName + " " + result[i].ownerSurname + ")\n");
+                            id = id + 1;
+                        }
+                    }
+                    if (dersler.length == 0) {
+                        alert("aktif ders yok");
+                        return;
+                    }
+                    var selectedDers = prompt("Seçim yapınız (sadece rakam girin):\n\n" + dersText);
+                    var ders = dersler[parseInt(selectedDers) - 1];
+                    $.ajax({
+                        url: "https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//livelesson/instudytime/join",
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                            "Accept": "json"
+                        },
+                        data: "studytimeid=" + ders.id + "&tokentype=nonce&platform=windows",
+                        withCredentials: true,
+                        crossDomain: true,
+                        xhrFields: {
+                            withCredentials: true
+                        },
+                        dataType: "json",
+                        success: function(resp2) {
+                            if (resp2.success == false) {
+                                alert("bir hata oluştu: " + resp2.operationMessage.replace('studytimenotstarted', 'ders daha başlamadı.'));
+                                return;
+                            }
+
+                            $.ajax({
+                                url: "https://cagriari.com/eba_nonceproxy.php?nonce="+resp2.meeting.token,
+                                success: function(resp3) {
+                                    try{ ga('send', 'event', {
+                                        eventCategory: "liveLesson",
+                                        eventAction: "join",
+                                        eventLabel: ""
+                                    }); }catch(a){}
+                                    window.location = encodeURI(resp2.meeting.url) + "?tk=" + encodeURIComponent(resp3.substring(1).split('|')[0]).replace('%26pwd%3D', '&pwd=');
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
     } else {
         $.ajax({
